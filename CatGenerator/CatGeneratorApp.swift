26b262248ba5f0be068f6708418f7f976d76d11a
Erica Sampson <ericabcherry@gmail.com>
2024-10-21 08:51:35 -0400

    Initial Commit

---
+++ CatGenerator/CatGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatGeneratorApp.swift
+//  CatGenerator
+//
+//  Created by Erica Sampson on 2024-10-21.
+//
+
+import SwiftUI
+
+@main
+struct CatGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
